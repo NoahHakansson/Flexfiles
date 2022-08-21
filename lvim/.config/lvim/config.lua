e854140d67c8e23668f2d3c736285100a912eb76
Noah Hakansson <noah.hakansson@protonmail.com>
2022-08-21 14:34:37 +0200

    fixed lsp_signature

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -328,7 +328,7 @@ lvim.builtin.which_key.mappings["?"] = {
 -- unbind <M-x> in insert mode
 -- vim.api.nvim_set_keymap("i", "<M-x>", "", {noremap = true})
 -- lsp_signature cfg
-local cfg = {
+local signature_cfg = {
   debug = false, -- set to true to enable debug logging
   log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
   -- default is  ~/.cache/nvim/lsp_signature.log
@@ -336,10 +336,15 @@ local cfg = {
 
   bind = true, -- This is mandatory, otherwise border config won't get registered.
   -- If you want to hook lspsaga or other signature handler, pls set to false
+
   doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
   -- set to 0 if you DO NOT want any API comments be shown
   -- This setting only take effect in insert mode, it does not affect signature help in normal
   -- mode, 10 by default
+
+  max_height = 12, -- max height of signature floating_window
+  max_width = 80, -- max_width of signature floating_window
+  wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
 
   floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
@@ -348,17 +353,14 @@ local cfg = {
   -- this setting will be helpful if you do not want the PUM and floating win overlap
 
   floating_window_off_x = 1, -- adjust float windows x position.
-  floating_window_off_y = 1, -- adjust float windows y position.
+  floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
 
-
+  close_timeout = 4000, -- close floating window after ms when laster parameter is entered
   fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
   hint_enable = true, -- virtual hint enable
-  hint_prefix = "🐼 ", -- Panda for parameter
+  hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
   hint_scheme = "String",
   hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-  max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
-  -- to view the hiding contents
-  max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
   handler_opts = {
     border = "rounded" -- double, rounded, single, shadow, none
   },
@@ -375,15 +377,18 @@ local cfg = {
   shadow_blend = 36, -- if you using shadow as border use this set the opacity
   shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
   timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  toggle_key = 'nil' -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+  toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+
+  select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
+  move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
 }
 
 -- load telescope extensions
-lvim.builtin.telescope.on_config_done = function(telescope)
-  -- pcall(telescope.load_extension, "frecency")
-  -- pcall(telescope.load_extension, "neoclip")
-  -- any other extensions loading
-end
+-- lvim.builtin.telescope.on_config_done = function(telescope)
+-- pcall(telescope.load_extension, "frecency")
+-- pcall(telescope.load_extension, "neoclip")
+-- any other extensions loading
+-- end
 
 -- Additional Plugins
 lvim.plugins = {
@@ -416,11 +421,8 @@ lvim.plugins = {
   {
     "ray-x/lsp_signature.nvim",
     config = function()
-      require "lsp_signature".on_attach()
-      require "lsp_signature".insert()
-      require "lsp_signature".setup(cfg)
-    end,
-    event = "BufRead"
+      require "lsp_signature".setup(signature_cfg)
+    end
   },
   { "ellisonleao/glow.nvim" },
   -- Github copilot
@@ -503,3 +505,4 @@ lvim.plugins = {
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
+
