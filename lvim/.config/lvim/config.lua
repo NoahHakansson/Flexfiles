a7c713abc98c2b00a060a59d5e83ac3757c794ca
Noah Hakansson <noah.hakansson@protonmail.com>
2022-11-30 13:46:55 +0100

    fixes,+ waybar spotify module

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -651,18 +651,6 @@ lvim.plugins = {
       require("substitute").setup()
     end
   },
-  -- {
-  --   "lukas-reineke/indent-blankline.nvim",
-  --   event = "BufRead",
-  --   setup = function()
-  --     vim.g.indentLine_enabled = 1
-  --     vim.g.indent_blankline_char = "▏"
-  --     vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "alpha" }
-  --     vim.g.indent_blankline_buftype_exclude = { "terminal" }
-  --     vim.g.indent_blankline_show_trailing_blankline_indent = false
-  --     vim.g.indent_blankline_show_first_indent_level = false
-  --   end
-  -- },
   -- half buggy colorizer
   {
     "norcalli/nvim-colorizer.lua",
