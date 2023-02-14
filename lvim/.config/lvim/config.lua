9c0598f1b9110a9ad8588d7e4e0c1fd95d074670
Noah Hakansson <noah.hakansson@protonmail.com>
2023-02-14 16:08:10 +0100

    fix

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -55,8 +55,8 @@ lvim.keys.visual_mode["<space>y"] = '"+y'
 -- shift+q unbind
 lvim.keys.normal_mode["<S-q>"] = ""
 -- step through buffers
-lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":bprev<CR>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- vertical resize of buffer windows
 lvim.keys.normal_mode["<S-Right>"] = ":vertical resize +2px<CR>"
 lvim.keys.normal_mode["<S-Left>"] = ":vertical resize -2px<CR>"
@@ -433,10 +433,15 @@ formatters.setup {
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
     filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
   },
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "prettier",
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
+  },
 }
 
 -- set additional linters
-local null_ls = require "null-ls"
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   { name = "revive", }, -- Golang linter
@@ -500,7 +505,6 @@ local signature_cfg = {
   log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
   -- default is  ~/.cache/nvim/lsp_signature.log
   verbose = false, -- show debug line number
-
   bind = true, -- This is mandatory, otherwise border config won't get registered.
   -- If you want to hook lspsaga or other signature handler, pls set to false
 
@@ -512,16 +516,13 @@ local signature_cfg = {
   max_height = 12, -- max height of signature floating_window
   max_width = 80, -- max_width of signature floating_window
   wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
-
   floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
-
   floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
   -- will set to true when fully tested, set to false will use whichever side has more space
   -- this setting will be helpful if you do not want the PUM and floating win overlap
 
   floating_window_off_x = 1, -- adjust float windows x position.
   floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
-
   close_timeout = 4000, -- close floating window after ms when laster parameter is entered
   fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
   hint_enable = true, -- virtual hint enable
@@ -531,21 +532,16 @@ local signature_cfg = {
   handler_opts = {
     border = "rounded" -- double, rounded, single, shadow, none
   },
-
   always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
-
   auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
   extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
   zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
-
   padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
-
   transparency = nil, -- disabled by default, allow floating win transparent value 1~100
   shadow_blend = 36, -- if you using shadow as border use this set the opacity
   shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
   timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
   toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
-
   select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
   move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
 }
@@ -667,7 +663,7 @@ lvim.plugins = {
         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
         -- Available modes: foreground, background
-        mode = 'background'; -- Set the display mode.
+        mode = 'background', -- Set the display mode.
       })
     end,
   },
