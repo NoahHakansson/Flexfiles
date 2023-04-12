e352232c4197903808900ff428b34c3faa86031d
Noah Hakansson <noah.hakansson@protonmail.com>
2023-04-12 21:03:09 +0200

    convert old vim autocommands to nvim lua autocommand

--- lvim/.config/lvim/lua/user/autocommands.lua
+++ lvim/.config/lvim/lua/user/autocommands.lua
@@ -2,37 +2,32 @@
 -- Autocommands
 --
 
+local generalAutoCmds = vim.api.nvim_create_augroup("General autocmds", { clear = true })
+
 -- Enable spell checking only for markdown files
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = { "*.md" },
 	command = "setlocal spell spelllang=en_us",
+	group = generalAutoCmds,
 })
 
 -- kitty terminal config auto reload on save
-vim.cmd("autocmd bufwritepost ~/.config/kitty/kitty.conf :silent !kill -SIGUSR1 $(pgrep kitty)")
-vim.cmd("autocmd bufwritepost ~/Flexfiles/kitty/.config/kitty/kitty.conf :silent !kill -SIGUSR1 $(pgrep kitty)")
-
--- Auto hybrid line numbers
--- enable relative line numbers only for the focused buffer when in normal mode
-local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
-	pattern = "*",
-	group = augroup,
+local kittyAutoCmds = vim.api.nvim_create_augroup("Kitty autocmds", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "~/.config/kitty/kitty.conf", "~/Flexfiles/kitty/.config/kitty/kitty.conf" },
 	callback = function()
-		if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
-			vim.opt.relativenumber = true
-		end
+		vim.cmd("silent !kill -SIGUSR1 $(pgrep kitty)")
 	end,
+	group = kittyAutoCmds,
 })
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
-	pattern = "*",
-	group = augroup,
+
+-- disable auto comment on new line
+vim.api.nvim_create_autocmd("BufEnter", {
 	callback = function()
-		if vim.o.nu then
-			vim.opt.relativenumber = false
-			vim.cmd("redraw")
-		end
+		vim.opt.formatoptions:remove({ "c", "r", "o" })
 	end,
+	desc = "Disable New Line Comment",
+	group = generalAutoCmds,
 })
 
 -- let treesitter use bash highlight for zsh files as well
@@ -41,4 +36,28 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function()
 		require("nvim-treesitter.highlight").attach(0, "bash")
 	end,
+	group = generalAutoCmds,
+})
+
+-- Auto hybrid line numbers
+-- enable relative line numbers only for the focused buffer when in normal mode
+local autoHybridLines = vim.api.nvim_create_augroup("numbertoggle", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+	pattern = "*",
+	callback = function()
+		if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+			vim.opt.relativenumber = true
+		end
+	end,
+	group = autoHybridLines,
+})
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+	pattern = "*",
+	callback = function()
+		if vim.o.nu then
+			vim.opt.relativenumber = false
+			vim.cmd("redraw")
+		end
+	end,
+	group = autoHybridLines,
 })
